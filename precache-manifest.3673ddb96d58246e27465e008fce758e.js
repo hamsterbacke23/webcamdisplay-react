fba96aa6b314e3c2dbd02c8bc4f17443c8535887
Tobias Schmidt <hallo.tobias.schmidt@gmail.com>
2019-01-27 13:35:14 +0100

    Updates

---
+++ precache-manifest.3673ddb96d58246e27465e008fce758e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0884d80020364238531",
+    "url": "/webcamdisplay-react/static/js/runtime~main.f0884d80.js"
+  },
+  {
+    "revision": "ab2ea8c90c6d0cae6fd4",
+    "url": "/webcamdisplay-react/static/js/main.ab2ea8c9.chunk.js"
+  },
+  {
+    "revision": "69dad5d0ac3e14d18b73",
+    "url": "/webcamdisplay-react/static/js/1.69dad5d0.chunk.js"
+  },
+  {
+    "revision": "ab2ea8c90c6d0cae6fd4",
+    "url": "/webcamdisplay-react/static/css/main.4162da51.chunk.css"
+  },
+  {
+    "revision": "0987558b8a136608aa76ed55b5b076a1",
+    "url": "/webcamdisplay-react/index.html"
+  }
+];
